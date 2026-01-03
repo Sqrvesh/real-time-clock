b3de0d208ca24a3172683a0310533db94aeccaac
Sqrvesh <sarveshhh42@gmail.com>
2026-01-03 19:31:22 +0530

    add buttons with hover effects in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import './Navbar.css';
+import settingsIcon from '../assets/settings.png';
+import darkModeIcon from '../assets/dark_mode.png';  
 
 export function Navbar() {
   return(
     <div className="navbar">
       <div className="title">Real time clock</div>
+      <div className="nav-button nav-selected">Clock</div>
+      <div className="nav-button">Alarm</div>
+      <div className="nav-icon-container">
+        <img src={darkModeIcon} className="nav-icon"/>
+      </div>
+      <div className="nav-icon-container">
+        <img src={settingsIcon} className="nav-icon"/>
+      </div>
     </div>
   );
 }
